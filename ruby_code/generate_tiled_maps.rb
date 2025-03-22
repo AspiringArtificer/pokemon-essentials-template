8e2fa24fba0f3c622048f72daa2ddbf62c609c63
Artificer <aspiringartificer@gmail.com>
2025-03-22 09:54:22 -0500

    tiled_generators: cleaned up comments and filepaths

--- ruby_code/generate_tiled_maps.rb
+++ ruby_code/generate_tiled_maps.rb
@@ -1,17 +1,22 @@
-require "fileutils"
-require "nokogiri"
-require "nokogiri-pretty"
+require "fileutils" # needed for file operations
+require "nokogiri" # needed to parse/edit xml
+require "nokogiri-pretty" # needed to print file with clean formatting
 
-require_relative "../tools/eevee/rmxp/rgss"
+require_relative "../tools/eevee/rmxp/rgss" # needed to load ruby data files
 
-TILESETS_DATA = "../src/data/Tilesets.rb"
-MAPINFOS = "../src/data/MapInfos.Local.rb"
-TMS_TEMPLATE = "map_template.tmx"
+RUBY_DIR = File.expand_path(File.dirname(__FILE__)) + "/"
+TMS_TEMPLATE = RUBY_DIR + "map_template.tmx"
 
+ROOT_DIR = RUBY_DIR + "../"
+TILESETS_DATA = ROOT_DIR + "src/data/Tilesets.rb"
+MAPINFOS = ROOT_DIR + "src/data/MapInfos.Local.rb"
+
+# load a ruby file exported from rxdata by eevee
 def load_ruby(export_file)
   return (RPGFactory.new).evaluate(File.read(export_file))
 end
 
+# generate a Tiled tmx map file based on the ruby map data files
 def generate_tmx(data_file, tilesets, mapinfos, output_dir)
   # extract data
   map_data = load_ruby(data_file)
@@ -81,19 +86,24 @@ def generate_tmx(data_file, tilesets, mapinfos, output_dir)
   File.write(output_file, map_file.human)
 end
 
-def generate_maps(source_dir)
+# generate Tiled map files for all the Map data files in a directory
+def generate_maps(source_dir, output_dir)
   tileset = load_ruby(TILESETS_DATA)
   mapinfo = load_ruby(MAPINFOS)
+  # FileUtils doesn't properly handle relative paths
+  # source_dir = File.expand_path(source_dir) + "/"
+  # output_dir = File.expand_path(output_dir) + "/"
 
   data_dir = Dir.new(source_dir)
   data_dir.each_child do |data_file|
     if data_file.include? "Map" and data_file.include? "-"
       puts "Processing #{data_file}..."
-      generate_tmx(source_dir + data_file, tileset, mapinfo, "../src/tiled/maps/")
+      generate_tmx(source_dir + data_file, tileset, mapinfo, output_dir)
     end
   end
 end
 
-src_data = "../src/data/"
-
-generate_maps(src_data)
+#generate maps for default dirs
+src_data = ROOT_DIR + "src/data/"
+target_dir = ROOT_DIR + "src/tiled/maps/"
+generate_maps(src_data, target_dir)
--- ruby_code/generate_tiled_tilesets.rb
+++ ruby_code/generate_tiled_tilesets.rb
@@ -1,40 +1,44 @@
-require "fileutils"
-require "nokogiri"
-require "fastimage"
+require "fileutils" # needed for file operations
+require "nokogiri" # needed to parse/edit xml
+require "fastimage" # needed to determine image dimensions
 
 TILE_SIZE = 32
+RUBY_DIR = File.expand_path(File.dirname(__FILE__)) + "/"
+TSX_TEMPLATE = RUBY_DIR + "tileset_template.tsx"
 
-TSX_TEMPLATE = "tileset_template.tsx"
+ROOT_DIR = RUBY_DIR + "../"
+AUTOTILES_INPUT = ROOT_DIR + "src/assets/Tiled/Graphics/Autotiles/"
+AUTOTILES_OUTPUT = ROOT_DIR + "src/tiled/tilesets/autotiles/"
+TILESET_INPUT = ROOT_DIR + "src/assets/Graphics/Tilesets/"
+TILESET_OUTPUT = ROOT_DIR + "src/tiled/tilesets/"
 
-AUTOTILES_INPUT = "../src/assets/Tiled/Graphics/Autotiles/"
 AUTOTILES_ASSET_DIR = "../../../assets/Tiled/Graphics/Autotiles/"
-AUTOTILES_OUTPUT = "../src/tiled/tilesets/autotiles/"
-
-TILESET_INPUT = "../src/assets/Graphics/Tilesets/"
 TILESET_ASSET_DIR = "../../../src/assets/Graphics/Tilesets/"
-TILESET_OUTPUT = "../src/tiled/tilesets/"
 
-def generate_tsx(source_dir, image_rel, output_dir)
+def generate_tsx(image_file, tsx_to_image_path, output_dir)
+  tileset = Nokogiri::XML(File.open(TSX_TEMPLATE))
+  px_dim = FastImage.size(image_file)
+  tile_dim = px_dim.map { |x| x / TILE_SIZE }
+
+  tileset.at_xpath("tileset")["name"] = File.basename(image_file, ".png")
+  tileset.at_xpath("tileset")["tilecount"] = tile_dim[0] * tile_dim[1]
+  tileset.at_xpath("tileset")["columns"] = tile_dim[0]
+
+  tileset.at_xpath("tileset").at_xpath("image")["source"] = tsx_to_image_path + File.basename(image_file)
+  tileset.at_xpath("tileset").at_xpath("image")["width"] = px_dim[0]
+  tileset.at_xpath("tileset").at_xpath("image")["height"] = px_dim[1]
+
+  FileUtils.mkdir_p(output_dir)
+  File.write(output_dir + File.basename(image_file, ".png") + ".tsx", tileset)
+end
+
+def generate_tilesets(source_dir, image_rel, output_dir)
   image_dir = Dir.new(source_dir)
   image_dir.each_child do |image|
     puts "Processing #{image}..."
-
-    tileset = Nokogiri::XML(File.open(TSX_TEMPLATE))
-    px_dim = FastImage.size(source_dir + image)
-    tile_dim = px_dim.map { |x| x / TILE_SIZE }
-
-    tileset.at_xpath("tileset")["name"] = File.basename(image, ".png")
-    tileset.at_xpath("tileset")["tilecount"] = tile_dim[0] * tile_dim[1]
-    tileset.at_xpath("tileset")["columns"] = tile_dim[0]
-
-    tileset.at_xpath("tileset").at_xpath("image")["source"] = image_rel + image
-    tileset.at_xpath("tileset").at_xpath("image")["width"] = px_dim[0]
-    tileset.at_xpath("tileset").at_xpath("image")["height"] = px_dim[1]
-
-    FileUtils.mkdir_p(output_dir)
-    File.write(output_dir + File.basename(image, ".png") + ".tsx", tileset)
+    generate_tsx(source_dir + image, image_rel, output_dir)
   end
 end
 
-generate_tsx(TILESET_INPUT, TILESET_ASSET_DIR, TILESET_OUTPUT)
-generate_tsx(AUTOTILES_INPUT, AUTOTILES_ASSET_DIR, AUTOTILES_OUTPUT)
+generate_tilesets(TILESET_INPUT, TILESET_ASSET_DIR, TILESET_OUTPUT)
+generate_tilesets(AUTOTILES_INPUT, AUTOTILES_ASSET_DIR, AUTOTILES_OUTPUT)
