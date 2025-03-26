ab7117ed3a0cd3d3ec76e08093661aa76d93bafb
Artificer <aspiringartificer@gmail.com>
2025-03-25 22:15:52 -0500

    tiled: added decompiled maps and tilesets from demo

---
+++ src/tiled/tilesets/autotiles/Black.tsx
@@ -0,0 +1,6 @@
+<?xml version="1.0" encoding="ISO-8859-1"?>
+
+<tileset version="1.10" tiledversion="1.11.2" name="Black" tilewidth="32" tileheight="32" tilecount="48" columns="6">
+   <image source="../../../assets/Tiled/Graphics/Autotiles/Black.png" width="192" height="256"/>
+   <properties/>
+</tileset>
---
+++ src/tiled/tilesets/autotiles/Flowers2.tsx
@@ -0,0 +1,6 @@
+<?xml version="1.0" encoding="ISO-8859-1"?>
+
+<tileset version="1.10" tiledversion="1.11.2" name="Flowers2" tilewidth="32" tileheight="32" tilecount="48" columns="6">
+   <image source="../../../assets/Tiled/Graphics/Autotiles/Flowers2.png" width="192" height="256"/>
+   <properties/>
+</tileset>
